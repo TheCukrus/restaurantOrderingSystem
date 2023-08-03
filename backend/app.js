dfe726f6e5d4677a0b6a776eb3b7321f8ec6e071
Jevgenij <sssgustavsss@gmail.com>
2023-08-03 23:54:07 +0300

    create app.js file

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const logger = require("./utils/logger.js")
+const config = require("./utils/config.js")
+const cors = require("cors")
+const middleware = require("./utils/middleware.js")
+const controllerUser = require("./controllers/controllerUser.js")
+const controllerLogin = require("./controllers/controllerLogin.js")
+
+const app = express()
+
+mongoose.connect(config.MONGODB)
+    .then(() => logger.info("Connect to mongoDB"))
+    .catch((error) => logger.error("Error connecting to mongoDB", error.message))
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.tokenExtractor)
+
+app.use("/api/users", controllerUser)
+app.use("/api/login", middleware.tokenExtractor, controllerLogin)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
+
+//login admin
+//email email@gmail.com
+//password assddffgg
