869c85c9bfda52317ad18123624e38025e367258
Jevgenij <sssgustavsss@gmail.com>
2023-08-07 23:46:26 +0300

    create admin controller

---
+++ backend/controllers/controllerAdmin.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken")
+const express = require("express")
+const logger = require("../utils/logger.js")
+const modelUser = require("../models/modelUser.js")
+
+const controllerAdmin = express.Router()
+
+controllerAdmin.get("/", async (req, res) =>
+{
+    try
+    {
+        if (!req.token.id)
+        {
+            return res.status(401).json({ message: "Not authorized" })
+        }
+
+        const userRole = await modelUser.findById(req.token.id)
+
+        if (userRole.role !== "admin")
+        {
+            return res.status(403).json({ message: "Have no access" })
+        }
+
+
+        res.status(200).json({ message: "Access granted" })
+    }
+    catch (err)
+    {
+        logger.error(err)
+    }
+})
+
+module.exports = controllerAdmin
