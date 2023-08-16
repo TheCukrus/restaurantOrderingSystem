374d0e83ec160bd59844e04433603b589c452351
Jevgenij <sssgustavsss@gmail.com>
2023-08-16 14:52:25 +0300

    create component

---
+++ frontend/src/pages/loginRegister/LoginRegister.jsx
@@ -0,0 +1,16 @@
+import "./LoginRegister.css"
+import Login from "./Login.jsx"
+import Register from "./Register.jsx"
+
+const LoginRegister = () =>
+{
+
+  return (
+    <div className="login-register">
+      <Login />
+      <Register />
+    </div>
+  )
+}
+
+export default LoginRegister
