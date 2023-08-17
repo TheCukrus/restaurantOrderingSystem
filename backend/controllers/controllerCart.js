edfdb8a1d13e4e18cb5ebae366d58ee5f202d906
Jevgenij <sssgustavsss@gmail.com>
2023-08-17 23:21:46 +0300

    modify

--- backend/controllers/controllerCart.js
+++ backend/controllers/controllerCart.js
@@ -6,6 +6,7 @@ const logger = require("../utils/logger.js")
 
 const controllerCart = express.Router()
 
+//Get cart from database
 controllerCart.get("/", async (req, res) =>
 {
     try
@@ -25,6 +26,7 @@ controllerCart.get("/", async (req, res) =>
     }
 })
 
+//Get cart's dish ID's
 controllerCart.post("/details", async (req, res) =>
 {
     try
@@ -39,7 +41,7 @@ controllerCart.post("/details", async (req, res) =>
         const detailedDishes = await Promise.all(
             dishIDs.map(async (dishId) =>
             {
-                const dish = await modelMenu.findById(dishId)
+                const dish = await modelMenu.findById(dishId.ids)
                 return dish
             })
         )
@@ -54,6 +56,7 @@ controllerCart.post("/details", async (req, res) =>
     }
 })
 
+//Create or update cart
 controllerCart.post("/", async (req, res) =>
 {
     try
@@ -72,7 +75,7 @@ controllerCart.post("/", async (req, res) =>
         if (cart)
         {
             //Update the existing cart
-            cart.cartItems.push(...cartItems)
+            cart.cartItems = cartItems
             cart.totalPrice = totalPrice
             await cart.save()
         }
@@ -87,6 +90,97 @@ controllerCart.post("/", async (req, res) =>
         }
 
         res.status(201).json({ message: "Cart created or updated" })
+    }
+    catch (err)
+    {
+        logger.error(err)
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+//Update cart's item quantity
+controllerCart.put("/:itemId", async (req, res) =>
+{
+    try
+    {
+        const { itemId } = req.params
+        const { quantity } = req.body
+
+        if (!req.token.id)
+        {
+            return res.status(401).json({ message: "Token invalid" })
+        }
+
+        //Find the cart and update the quantity for the specific item
+        const userId = req.token.id
+        const cart = await modelCart.findOne({ userId })
+
+        const updatedCartItems = cart.cartItems.map((item) =>
+        {
+            return item.itemId.toString() === itemId ? { ...item, quantity } : item
+        }
+        )
+
+        cart.cartItems = updatedCartItems
+        await cart.save()
+
+        res.status(200).json({ message: "Quantity updated successfully" })
+    }
+    catch (err)
+    {
+        logger.error(err)
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+//Delete item from the cart
+controllerCart.delete("/:itemId", async (req, res) =>
+{
+    try
+    {
+        const { itemId } = req.params
+
+        if (!req.token.id)
+        {
+            return res.status(401).json({ message: "Token invalid" })
+        }
+
+        const userId = req.token.id
+        const cart = await modelCart.findOne({ userId })
+
+        const updatedCartItems = cart.cartItems.filter(
+            item => item.itemId.toString() !== itemId
+        )
+
+        cart.cartItems = updatedCartItems
+        await cart.save()
+
+        res.status(200).json({ message: "Item removed from cart successfully" })
+    }
+    catch (err)
+    {
+        logger.error(err)
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+//Delete whole cart
+controllerCart.delete("/", async (req, res) =>
+{
+    try
+    {
+        if (!req.token.id)
+        {
+            res.status(401).json({ message: "Token invalid" })
+        }
+
+        const userId = req.token.id
+        const cart = await modelCart.findOne({ userId })
+
+        cart.cartItems = []
+        await cart.save()
+
+        res.status(204).json({ message: "Cart cleared successfully" })
     }
     catch (err)
     {
