832f6f79ddad343c0079e5cab2919b4eb594df3e
Jevgenij <sssgustavsss@gmail.com>
2023-08-19 00:20:27 +0300

    modify component

--- frontend/src/pages/orderTracking/OrderTracking.jsx
+++ frontend/src/pages/orderTracking/OrderTracking.jsx
@@ -1,59 +1,66 @@
-import React, { useState } from 'react';
+import { useState } from "react"
+import OrderHistory from "./OrderHistory.jsx"
 import "./OrderTracking.css"
+import { useUserContext } from "../../contexts/UserContext.js"
+import orderService from "../../services/orderHistory.js"
 
-const OrderTracking = () => {
-    // Sample data for order tracking (you can replace this with data from your application state or backend)
-    const orderHistory = [
-      { id: '12345', status: 'Order Received' },
-      { id: '54321', status: 'Preparing' },
-      { id: '67890', status: 'Out for Delivery' },
-      // Add more order entries for testing
-    ];
-  
-    // State for user input
-    const [orderId, setOrderId] = useState('');
-    const [orderStatus, setOrderStatus] = useState(null);
-  
-    // Function to handle order tracking
-    const trackOrder = (e) => {
-      e.preventDefault();
-      // Find the order with the provided order ID
-      const trackedOrder = orderHistory.find(order => order.id === orderId);
-      // Update the order status
-      setOrderStatus(trackedOrder?.status || 'Order not found');
-    };
-  
-    return (
-      <div className="order-tracking">
-        <h2>Order Tracking</h2>
-        <form onSubmit={trackOrder}>
-          <label htmlFor="order-id">Enter Order ID:</label>
-          <input
-            type="text"
-            id="order-id"
-            value={orderId}
-            onChange={(e) => setOrderId(e.target.value)}
-          />
-          <button type="submit">Track Order</button>
-        </form>
-  
-        {/* Display order status */}
-        {orderStatus && (
-          <div className="order-status">
-            <h3>Order Status: {orderStatus}</h3>
-            {/* Add your timeline-based view here */}
-            {/* Example: */}
-            <ul>
-              <li>Order Received</li>
-              <li>Preparing</li>
-              <li>Out for Delivery</li>
-              <li>Delivered</li>
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default OrderTracking;
-  
+const OrderTracking = () =>
+{
+  const { user } = useUserContext()
+
+  // State for user input
+  const [orderId, setOrderId] = useState("");
+  const [orderStatus, setOrderStatus] = useState(null);
+
+  // Function to handle order tracking
+  const trackOrder = async (e) =>
+  {
+    e.preventDefault()
+    try
+    {
+      if (!orderId)
+      {
+        return
+      }
+
+      const data = await orderService.getOrder(orderId)
+      if (!data)
+      {
+        return setOrderStatus("Order not Found")
+      }
+      return setOrderStatus(data.data.status)
+    }
+    catch (err)
+    {
+      console.log(err)
+    }
+  }
+
+  return (
+    <div className="order-tracking">
+      <h2>Order Tracking</h2>
+      <form onSubmit={trackOrder}>
+        <label htmlFor="order-id">Enter Order ID:</label>
+        <input
+          type="text"
+          id="order-id"
+          value={orderId}
+          onChange={(e) => setOrderId(e.target.value)}
+        />
+        <button type="submit">Track Order</button>
+      </form>
+
+      {/* Display order status */}
+      {orderStatus && (
+        <div className="order-status">
+          <h3>Order Status: {orderStatus}</h3>
+        </div>
+      )}
+
+      {user && <h2>Order History</h2>}
+      <OrderHistory />
+    </div >
+  );
+};
+
+export default OrderTracking;
