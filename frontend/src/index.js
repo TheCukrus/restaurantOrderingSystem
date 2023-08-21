b5b49cef2a5feed9da382caa7722242caff2551a
Jevgenij <sssgustavsss@gmail.com>
2023-08-21 10:24:38 +0300

    modify index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
 import { BrowserRouter as Router } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import App from "./App"
+import { MenuProvider } from "./contexts/MenuContext.js"
+import { CartProvider } from "./contexts/CartContext.js"
+import { UserProvider } from "./contexts/UserContext.js"
+import { NotificationProvider } from "./contexts/NotificationContext.js"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <NotificationProvider>
+      <UserProvider>
+        <MenuProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </MenuProvider>
+      </UserProvider>
+    </NotificationProvider>
+  </Router>
 )
