d4ccef25b0a6c537e087ff04ae9d90c8e671cb87
Jevgenij <sssgustavsss@gmail.com>
2023-08-21 10:24:11 +0300

    modify app file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,23 +6,48 @@ import Cart from "./pages/cart/Cart.jsx"
 import MenuPage from "./pages/menuPage/MeniuPage.jsx"
 import OrderTracking from "./pages/orderTracking/OrderTracking.jsx"
 import LoginRegister from "./pages/loginRegister/LoginRegister"
-import { useState } from "react"
+import AdminPage from "./pages/admin/AdminPage.jsx"
+import { useUserContext } from "./contexts/UserContext.js"
+import UnmatchedRoute from "./pages/pageNotFound/UnmatchedRoute.jsx"
+import Notification from "./pages/notification/Notification.jsx"
+import "./App.css"
+import Checkout from "./pages/checkout/Checkout.jsx"
 
 const App = () =>
 {
 
-  const [user, setUser] = useState(null)
+  const { adminRights, user } = useUserContext()
 
   return (
-    <div>
-      <Navbar user={user} />
+    <div className="main-container">
+      <Navbar />
+      <Notification />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<MenuPage />} />
-        <Route path="/cart" element={<Cart />} />
+
+        {/* Checking if authorized user to react cart */}
+        {user
+          ? <Route path="/cart" element={<Cart />} />
+          : null
+        }
+
+        {user
+          ? <Route path="/checkout" element={<Checkout />} />
+          : null}
+
         <Route path="/login" element={<LoginRegister />} />
         <Route path="/tracking" element={<OrderTracking />} />
+
+        {/* Checking if user has rigth to enter admin page */}
+        {adminRights
+          ? (<Route path="/admin" element={<AdminPage />} />)
+          : null
+        }
+
+        {/* Create a catch-all route for unmatched paths */}
+        <Route path="*" element={<UnmatchedRoute />} />
       </Routes>
 
       <Footer />
