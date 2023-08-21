d369bf1f81c8cf1afdff9d71314f8c9980c17b74
Jevgenij <sssgustavsss@gmail.com>
2023-08-21 10:50:42 +0300

    modify app.js file

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,19 @@ const controllerMenu = require("./controllers/controllerMeniu.js")
 const controllerAdmin = require("./controllers/controllerAdmin.js")
 const controllerCart = require("./controllers/controllerCart.js")
 const controllerOrderHistory = require("./controllers/controllerOrderHistory.js")
+const controllerTestimonial = require("./controllers/controllerTestimonial.js")
+const controllerContact = require("./controllers/controllerContact.js")
+const path = require("path")
 
 const app = express()
+
+const frontendBuildPath = path.resolve(__dirname, "..", "frontend", "build")
 
 mongoose.connect(config.MONGODB)
     .then(() => logger.info("Connect to mongoDB"))
     .catch((error) => logger.error("Error connecting to mongoDB", error.message))
 
+app.use(express.static(frontendBuildPath))
 app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
@@ -27,6 +33,8 @@ app.use("/api/menu", middleware.tokenExtractor, controllerMenu)
 app.use("/api/admin", middleware.tokenExtractor, controllerAdmin)
 app.use("/api/cart", middleware.tokenExtractor, controllerCart)
 app.use("/api/orderHistory", middleware.tokenExtractor, controllerOrderHistory)
+app.use("/api/testimonial", middleware.tokenExtractor, controllerTestimonial)
+app.use("/api/contact", middleware.tokenExtractor, controllerContact)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
