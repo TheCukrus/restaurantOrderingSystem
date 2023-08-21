2f1ac368ac9d9057a8a58876288d5c9c199e2391
Jevgenij <sssgustavsss@gmail.com>
2023-08-21 15:22:26 +0300

    modify order history service

--- frontend/src/services/orderHistory.js
+++ frontend/src/services/orderHistory.js
@@ -1,6 +1,15 @@
 import axios from "axios"
 
-const baseUrl = "http://127.0.0.1:80/api/orderHistory"
+let baseUrl
+
+if (process.env.NODE_ENV === "development")
+{
+    baseUrl = "http://127.0.0.1:80/api/orderHistory"
+}
+else
+{
+    baseUrl = "https://restaurantordersystem.onrender.com/api/orderHistory"
+}
 
 const getToken = () =>
 {
