6a46817825a949fe025fe5971a9e9153990e7c33
Jevgenij <sssgustavsss@gmail.com>
2023-08-21 10:21:48 +0300

    create cart service

---
+++ frontend/src/services/cart.js
@@ -0,0 +1,102 @@
+import axios from "axios"
+
+const baseUrl = "http://127.0.0.1:80/api/cart"
+
+const getToken = () =>
+{
+    const tokenFromStorage = window.localStorage.getItem("token")
+    return tokenFromStorage ? `Bearer ${JSON.parse(tokenFromStorage).token}` : null
+}
+
+const setAuthorizationToken = (token) =>
+{
+    config.headers.authorization = token
+}
+
+const config = {
+    headers: {
+        "authorization": getToken()
+    }
+}
+
+const getCart = async () =>
+{
+
+    try
+    {
+        const cartData = await axios.get(baseUrl, config)
+        return cartData.data
+    }
+    catch (err)
+    {
+        console.log(err)
+    }
+}
+
+const getCartDishes = async (data) =>
+{
+    try
+    {
+        const details = await axios.post(`${baseUrl}/details`, data, config)
+        return details.data
+    }
+    catch (err)
+    {
+        console.log(err)
+    }
+}
+
+const updateCart = async (data) =>
+{
+    try
+    {
+        const createCart = await axios.post(baseUrl, data, config)
+        return createCart
+    }
+    catch (err)
+    {
+        console.log(err)
+    }
+}
+
+const updateCartItemQuantity = async (itemId, newQuantity) =>
+{
+    try
+    {
+        const response = await axios.put(`${baseUrl}/${itemId}`, { quantity: newQuantity }, config)
+        return response.data
+    }
+    catch (err)
+    {
+        console.log(err)
+    }
+}
+
+const removeCartItem = async (itemId) =>
+{
+    try
+    {
+        const response = await axios.delete(`${baseUrl}/${itemId}`, config)
+        return response.data
+    }
+    catch (err)
+    {
+        console.log(err)
+    }
+}
+
+const removeAll = async () =>
+{
+    try
+    {
+        const response = await axios.delete(baseUrl, config)
+        return response.data
+    }
+    catch (err)
+    {
+        console.log(err)
+    }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { updateCart, getCart, getCartDishes, setAuthorizationToken, updateCartItemQuantity, removeCartItem, removeAll }
