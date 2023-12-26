66c15dd4f0f6377881c520e2ceb7cf5597f1ae09
Jevgenij <sssgustavsss@gmail.com>
2023-12-26 12:07:32 +0200

    modify

--- frontend/src/pages/navbar/Navbar.jsx
+++ frontend/src/pages/navbar/Navbar.jsx
@@ -1,67 +1,49 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import "./Navbar.css"
-import { useUserContext } from "../../contexts/UserContext.js"
-import { useCartContext } from "../../contexts/CartContext.js"
-import { Badge } from "react-bootstrap"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { useUserContext } from "../../contexts/UserContext.js";
+import { useCartContext } from "../../contexts/CartContext.js";
+import { Badge } from "react-bootstrap";
 
-const Navbar = () =>
-{
+const Navbar = () => {
+    const { user, setUser, adminRights, setAdminRights } = useUserContext();
+    const { cartItems } = useCartContext();
 
-    const { user, setUser, adminRights, setAdminRights } = useUserContext()
-    const { cartItems } = useCartContext()
+    const [showDropdown, setShowDropdown] = useState(false);
 
-    const [showDropdown, setShowDropdown] = useState(false)
-
-    const toggleDropdown = () =>
-    {
-        setShowDropdown(!showDropdown)
-    }
-
-    const closeDropDown = () =>
-    {
-        setShowDropdown(false)
-    }
-
-    const logout = () =>
-    {
-        window.localStorage.removeItem("token")
-        setAdminRights(false)
-        setUser(null)
-        return
-    }
+    const logout = () => {
+        window.localStorage.removeItem("token");
+        setAdminRights(false);
+        setUser(null);
+    };
 
     return (
         <nav className="navbar">
             <div className="navbar-logo">
                 <Link to="/">
-                    <img src="/images/restaurantLogo.png" alt="Restaurant Logo" />
+                    <img src="/images/restaurantLogo.png" alt="Restaurant Logo" className="navbar-logo-img" />
                 </Link>
             </div>
+            <div className={`navbar-menu ${showDropdown ? 'active' : ''}`}>
+                <Link to="/menu" className="navbar-link">Menu</Link>
+                <Link to="/tracking" className="navbar-link">Order Tracking</Link>
+                {user ? (
+                    <>
+                        {adminRights && <Link to="/admin" className="navbar-link">Admin</Link>}
+                        <Link to="/cart" className="navbar-link">
+                            Cart <Badge pill bg="secondary">{cartItems.length}</Badge>
+                        </Link>
+                        <Link to="/" onClick={logout} className="navbar-link">Logout</Link>
+                    </>
+                ) : (
+                    <Link to="/login" className="navbar-link">Login/Register</Link>
+                )}
+                <button className="navbar-toggler" onClick={() => setShowDropdown(!showDropdown)}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    );
+};
 
-            <button className="navbar-toggler" onClick={toggleDropdown}>
-                <img src="/icons/hamburger.svg" alt="Toggle Navigation" />
-            </button>
-
-            <ul className={`navbar-links${showDropdown ? " show" : ""}`}>
-                <li><Link to="/menu" onClick={closeDropDown}>Menu</Link></li>
-                <li><Link to="/tracking" onClick={closeDropDown}>Order Tracking</Link></li>
-                {!user
-                    ? (<li><Link to="/login" onClick={closeDropDown}>Login/Register</Link></li>)
-                    : (
-                        <>
-                            {adminRights
-                                ? (<li><Link to="/admin" onClick={closeDropDown}>Admin</Link></li>)
-                                : null
-                            }
-                            <li><Link to="/cart" onClick={closeDropDown} >Cart {cartItems.length > 0 ? <Badge pill bg="secondary">{cartItems.length}</Badge> : ""}</Link></li>
-                            <li> <Link to="/" onClick={() => { closeDropDown(); logout(); }}>Logout</Link></li>
-                        </>
-                    )
-                }
-            </ul>
-        </nav >
-    )
-}
-
-export default Navbar
+export default Navbar;
